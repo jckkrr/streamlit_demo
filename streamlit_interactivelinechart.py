395c2dc2bf8335afc0f5a9fce860dbcd5ca78c6f
Jack Kerr <69304112+jckkrr@users.noreply.github.com>
2023-01-23 10:57:58 +1100

    Add files via upload

---
+++ streamlit_interactivelinechart.py
@@ -0,0 +1,17 @@
+### streamlit run "C:\Users\Jack\Documents\Python_projects\2023\streamlit\streamlit_interactivelinechart.py"
+
+import pandas as pd
+import streamlit as st
+
+import os
+
+import random
+    
+df = pd.DataFrame(columns=['x','y'])
+x = [x for x in range(1,11)]
+power_n = st.slider('Power N', 0, 10, 1)
+
+df['x'] = x
+df['y'] = [x ** power_n for x in x]
+
+st.line_chart(df, x='x', y='y')
