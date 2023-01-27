e5b151ad51a90c7a7833fba64a3d9dd15d87f9dc
Jack Kerr <69304112+jckkrr@users.noreply.github.com>
2023-01-27 19:18:23 +1100

    Add files via upload

--- test.py
+++ test.py
@@ -1,26 +1,28 @@
 ## streamlit run "C:\Users\Jack\Documents\Python_projects\2023\streamlit\test.py"
 
-import plotly.express as px
+import pandas as pd
+import plotly.graph_objects as go
+import networkx as nx
 import streamlit as st
+import streamlit.components.v1 as components
+import pyvis
 
-df = px.data.gapminder()
+df = pd.DataFrame(columns=['source', 'target'])
 
-fig = px.scatter(
-    df.query("year==2007"),
-    x="gdpPercap",
-    y="lifeExp",
-    size="pop",
-    color="continent",
-    hover_name="country",
-    log_x=True,
-    size_max=60,
-)
+df.loc[0] = 'Harry', 'Megan'
+df.loc[1] = 'Harry', 'William'
+df.loc[2] = 'William', 'Kate'
 
-tab1, tab2 = st.tabs(["Streamlit theme (default)", "Plotly native theme"])
-with tab1:
-    # Use the Streamlit theme.
-    # This is the default. So you can also omit the theme argument.
-    st.plotly_chart(fig, theme="streamlit", use_container_width=True)
-with tab2:
-    # Use the native Plotly theme.
-    st.plotly_chart(fig, theme=None, use_container_width=True)
+G = nx.from_pandas_edgelist(df, 'source', 'target', None)
+
+fig = pyvis.network.Network(height=1000, width=1000, directed=False)
+
+fig.from_nx(G)
+
+#st.plotly_chart(fig, theme=None, use_container_width=True)
+
+path = '/tmp'
+fig.save_graph(f'temp.html')
+HtmlFile = open(f'temp.html', 'r', encoding='utf-8')
+
+components.html(HtmlFile.read(), height=435)
