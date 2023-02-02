7757745504fc7f7686e7f95c966ad3bccb92773c
Jack Kerr <69304112+jckkrr@users.noreply.github.com>
2023-02-02 15:57:10 +1100

    Add files via upload

---
+++ streamlit-app.py
@@ -0,0 +1,17 @@
+import pandas as pd
+import streamlit as st
+
+import os
+
+import random
+    
+df = pd.DataFrame(columns=['x','y'])
+x = [x for x in range(1,11)]
+power_n = st.slider('Power N', 0, 10, 1)
+
+df['x'] = x
+df['y'] = [x ** power_n for x in x]
+
+st.line_chart(df, x='x', y='y')
+
+st.write('hi')
